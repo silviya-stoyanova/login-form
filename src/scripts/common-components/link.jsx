d6ad35db0b277257cfdffbf558e9f375ed49cd9f
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2021-04-29 13:27:39 +0300

    Added default class name to Link.jsx

--- src/scripts/common-components/link.jsx
+++ src/scripts/common-components/link.jsx
@@ -3,12 +3,14 @@ import '../../styles/common-components/link.scss';
 import React from 'react';
 
 const Link = ({ href, text, children, className }) => {
+    const combinedClassName = `link ${className}`;
+
     const content = !!text
         ? text
         : children;
 
     return (
-        <a href={href} className={className} >
+        <a href={href} className={combinedClassName} >
             {content}
         </a>
     )
