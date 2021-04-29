ee9f6ad5a6d9987297154fcde3c81239281cf184
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2021-04-29 13:04:15 +0300

    Added event handlers

--- src/scripts/login/login-form.jsx
+++ src/scripts/login/login-form.jsx
@@ -1,29 +1,53 @@
 import '../../styles/login/login-form.scss';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { strings } from '../constants/constants';
 
-const { email, password, login } = strings;
+const {
+    email: emailString,
+    password: passwordString,
+    login
+} = strings;
 
-const LoginForm = () => (
-    <form action="#" className="login-form" method="">
-        <input
-            className="login-form-field"
-            placeholder={email}
-        />
-        <input
-            className="login-form-field"
-            placeholder={password}
-        />
-        <button
-            className="login-form-button"
-            type="submit"
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleFieldOnBlur = ({ target }) => {
+
+    };
+
+    const handleSubmitBtnClick = ({ target }) => {
+        alert(`Wellcome, ${email}`);
+    };
+
+    return (
+        <form
+            className="login-form"
+            action="#"
+            method=""
         >
-            <span className="login-form-button-text">
-                {login}
-            </span>
-        </button>
-    </form>
-);
+            <input
+                className="login-form-field"
+                placeholder={emailString}
+                onBlur={handleFieldOnBlur}
+            />
+            <input
+                className="login-form-field"
+                placeholder={passwordString}
+                onBlur={handleFieldOnBlur}
+            />
+            <button
+                className="login-form-button"
+                type="submit"
+                onClick={handleSubmitBtnClick}
+            >
+                <span className="login-form-button-text">
+                    {login}
+                </span>
+            </button>
+        </form>
+    );
+}
 
 export default LoginForm;
